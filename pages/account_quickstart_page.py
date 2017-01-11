645775cd32194d406b88d16a043b868f9a4f1d46
Mirosław Zalewski <mz@miroslaw-zalewski.eu>
2017-01-11 14:42:02 +0100

    Fix issue with another element receiving click event for good

--- pages/account_quickstart_page.py
+++ pages/account_quickstart_page.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 
 from pages.abstract.weather_page import WeatherPage
@@ -13,7 +13,8 @@ class AccountQuickstartPage(WeatherPage):
         "home_address_input": (By.ID, 'homeLocation'),
         "address_autocomplete_item": (By.CSS_SELECTOR, '#quickstartForm ul.dropdown-menu li'),
         "home_address_valid": (By.CSS_SELECTOR, '#quickstartForm div.form-control-wrapper.form-control-valid'),
-        "proceed_button": (By.CSS_SELECTOR, '#quickstartForm button[type=submit]')
+        "proceed_button": (By.CSS_SELECTOR, '#quickstartForm button[type=submit]'),
+        "whats_new_close_button": (By.CSS_SELECTOR, 'div[modal-render] button.close > span'),
     })
 
     def quick_setup_account(self, user):
@@ -28,5 +29,14 @@ class AccountQuickstartPage(WeatherPage):
 
         self.driver.switch_to.default_content()
 
-        self.wait_for_visibility_of_element(self.find_element(self.locator_dictionary["profile_button"]))
+        # After creating account, modal window is displayed.
+        # Except that there are actually two copies of the same window, one on top of another
+        self.wait_for_element_to_be_clickable(self.locator_dictionary["whats_new_close_button"])
+        for elem in self.find_elements(self.locator_dictionary["whats_new_close_button"])[::-1]:
+            try:
+                elem.click()
+            except WebDriverException:
+                pass
+
+        self.wait_for_element_to_be_clickable(self.locator_dictionary["profile_button"])
         return MainPage(self.driver)
